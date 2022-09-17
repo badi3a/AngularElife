e654311fff8c20782ba4b5d7b4ec92f488c39c16
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-09-17 16:39:52 +0200

    tsConfig

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-product.component.css']
 })
 export class ListProductComponent implements OnInit {
-
-  constructor() { }
-
+  public title: string;
+  constructor() {
+  }
   ngOnInit(): void {
+    this.title = 'Myshop';
   }
 
 }
