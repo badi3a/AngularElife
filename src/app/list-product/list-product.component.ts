61fe09443a4fadeaeaf8464a6961a3f29fe7a484
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-09-24 11:10:30 +0200

    update list product component

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-list-product',
@@ -7,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
   public title: string;
+  public list: Product[];
   constructor() {
   }
   ngOnInit(): void {
-    this.title = 'Myshop';
+    this.title = 'MyShop';
+    this.list=[
+       {id:12,
+        name: 'T-shirt 1',
+        price: 25,
+        nbrLike: 10,
+        quantity: 3,
+        description: 'nice Tshirt',
+        picture: 'https://www.exist.com.tn/83302-home_default/t-shirt.jpg'
+      },
+      {id:13,
+        name: 'T-shirt 3',
+        price: 20,
+        nbrLike: 10,
+        quantity: 0,
+        description: 'nice Tshirt',
+        picture: 'https://www.exist.com.tn/61485-large_default/t-shirt.jpg'
+      }
+    ]
   }
 
 }
---
+++ src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product{
+  id: number;
+  name: String;
+  price: number;
+  picture: String;
+  nbrLike: number;
+  description: String;
+  quantity: number;
+}
